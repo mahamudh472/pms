b222d9cb63e2c2c51861c8fece3a8797d7902b69
Mahmud Hasan <106822443+mahamudh472@users.noreply.github.com>
2023-05-23 16:55:17 -0700

    Add files via upload

--- product_management/urls.py
+++ product_management/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from . import views
@@ -28,4 +30,4 @@ urlpatterns = [
     path('activeorders', views.Main_view.activeOrders, name='activeorders'),
     path('order', views.Main_view.order, name='order'),
 
-]
+] + static('/static/', document_root=settings.MEDIA_ROOT)
